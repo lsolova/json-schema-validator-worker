5be03eef7224bf7707c5d766d532f1b51d352f90
Laszlo Solova <laszlo@solova.com>
2026-03-20 14:27:45 +0100

    Issue 19: Fix missing window in WASM Running validator in Node.js was panicking because window in the underlying window.fetch was undefined and unwrapped without error handling. A fallback to Reqwest is implemented.

--- node-example.ts
+++ node-example.ts
@@ -6,17 +6,35 @@ async function validateWithWasm() {
     const wasmBuffer = await readFile("./dist/assets/schema_validator.wasm");
     await SchemaValidator.init(wasmBuffer);
 
-    const schema = await readFile("./public/test-node.schema.json", {
+    const schema1 = await readFile("./public/test-node.schema.json", {
       encoding: "utf8",
     });
-    const data = {
+    const schema2 = await readFile("./public/email-metadata.schema.json", {
+      encoding: "utf8"
+    });
+
+    await SchemaValidator.registerSchema("https://example.com/schemas/email-metadata.schema.json", schema2);
+
+    const data1 = {
       name: "John",
       age: 30,
       email: "john@example.com",
     };
 
-    await SchemaValidator.validate(schema, data);
-    console.info("Schema is valid.");
+    const data2 = {
+      from: "john@example.com",
+      to: "jane@example.com",
+      subject: "Test email"
+    }
+
+    await SchemaValidator.validate(schema1, data1);
+    console.info("Data 1 is valid.");
+
+    await SchemaValidator.validate("https://example.com/schemas/email-metadata.schema.json", data2);
+    console.info("Data 2 is valid.");
+
+    await SchemaValidator.validate("https://json-schema.org/draft/2020-12/schema", schema2);
+    console.info("Schema 2 is valid.");
   } catch (error) {
     console.error("Failed", error);
   }
--- wasm/src/schema_store/store_utils.rs
+++ wasm/src/schema_store/store_utils.rs
@@ -1,14 +1,37 @@
-use std::error::Error;
 use serde_json::Value;
+use std::error::Error;
+
+async fn retrieve_via_http_reqwest(uri: &str) -> Result<Value, Box<dyn Error + Send + Sync>> {
+    use reqwest::{get, Response};
+
+    let resp: Response = get(uri).await?;
+    match resp.json::<Value>().await {
+        Ok(schema) => Ok(schema),
+        Err(e) => Err(Into::into(e)),
+    }
+}
 
 pub async fn retrieve_via_http(uri: &str) -> Result<Value, Box<dyn Error + Send + Sync>> {
     #[cfg(target_family = "wasm")]
     {
         use wasm_bindgen::{JsCast, JsValue};
         use wasm_bindgen_futures::JsFuture;
-        use web_sys::{window, Response};
+        use web_sys::{console, window, Response};
 
-        let window = window().unwrap();
+        let window = match window() {
+            Some(w) => w,
+            None => {
+                console::warn_1(&JsValue::from_str(
+                    format!(
+                        "Window is undefined. Fallback to custom fetch when retrieve '{}'",
+                        &uri
+                    )
+                    .as_str(),
+                ));
+                return retrieve_via_http_reqwest(uri).await;
+            }
+        };
+
         let result: JsValue = JsFuture::from(window.fetch_with_str(uri))
             .await
             .map_err(|e| format!("Fetch failed: {:?}", e))?;
@@ -32,11 +55,6 @@ pub async fn retrieve_via_http(uri: &str) -> Result<Value, Box<dyn Error + Send 
 
     #[cfg(not(target_family = "wasm"))]
     {
-        use reqwest::{get, Response};
-        let resp: Response = get(uri).await?;
-        match resp.json::<Value>().await {
-            Ok(schema) => Ok(schema),
-            Err(e) => Err(Into::into(e)),
-        }
+        return retrieve_via_http_reqwest(uri)?;
     }
 }
